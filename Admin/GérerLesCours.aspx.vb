52a5e546af0ece668be423f0085bec96ff25a104
Nyrthak <k.vandal@hotmail.ca>
2012-10-01 14:01:12 -0400

    Page Admin gérer cours (modifier en multiview)

--- Admin/GérerLesCours.aspx.vb
+++ Admin/GérerLesCours.aspx.vb
@@ -5,8 +5,20 @@ Partial Class Admin_GérerLesCours
 
     Private Shared lecontext As ModelContainer = Nothing
     Protected Sub page_load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
         lecontext = New ModelContainer()
+    End Sub
+
+    Protected Sub dsContextCreating(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.EntityDataSourceContextCreatingEventArgs) _
+        Handles EntityDataSourceCategorie.ContextCreating, EntityDataSourceGroupeDAge.ContextCreating, EntityDataSourcePrerequis.ContextCreating, EntityDataSourceSession.ContextCreating
+        'RÉCUPÈRE LE CONTEXTE DE FACON À N'EN AVOIR QU'UN
+        If Not lecontext Is Nothing Then
+            e.Context = lecontext
+        End If
+    End Sub
+
+    Protected Sub dsContextDisposing(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.EntityDataSourceContextDisposingEventArgs) _
+       Handles EntityDataSourceCategorie.ContextDisposing, EntityDataSourceGroupeDAge.ContextDisposing, EntityDataSourcePrerequis.ContextDisposing, EntityDataSourceSession.ContextDisposing
+        e.Cancel = True
     End Sub
 
     Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
@@ -30,8 +42,8 @@ Partial Class Admin_GérerLesCours
         leCoursAjouté.noCoursRequis = 0
         lecontext.AddObject("CoursSet", leCoursAjouté)
         lecontext.SaveChanges()
-        Dim url As String = "~/Admin/ModifierCours.aspx?noCours=" & leCoursAjouté.noCours
-        Response.Redirect(url)
+        hFieldNoCours.Value = leCoursAjouté.noCours
+        mViewCours.ActiveViewIndex = 1
     End Sub
 
     Protected Sub lViewCours_ItemCommand(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ListViewCommandEventArgs) Handles lViewCours.ItemCommand
@@ -41,6 +53,30 @@ Partial Class Admin_GérerLesCours
             lecontext.CoursSet.DeleteObject(leCoursADeleter)
             lecontext.SaveChanges()
             Response.Redirect("~/Admin/GérerLesCours.aspx")
+        ElseIf e.CommandName = "Modifier" Then
+            hFieldNoCours.Value = e.CommandArgument
+            lViewModifierCours.EditIndex = 0
+            mViewCours.ActiveViewIndex = 1
         End If
+
     End Sub
+
+    Protected Sub lViewModifierCours_ItemCommand(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ListViewCommandEventArgs) Handles lViewModifierCours.ItemCommand
+        If e.CommandName = "Annuler" Then
+            Dim leNoCours As String = hFieldNoCours.Value
+            Dim leCoursADeleter As Cours = (From monCours In lecontext.CoursSet Where monCours.noCours = leNoCours Select monCours).First
+            If leCoursADeleter.Nom = "Entrez un nom" Then
+                lecontext.CoursSet.DeleteObject(leCoursADeleter)
+                lecontext.SaveChanges()
+                mViewCours.ActiveViewIndex = 0
+            End If
+        End If
+        mViewCours.ActiveViewIndex = 0
+    End Sub
+
+    Protected Sub lViewModifierCours_ItemUpdated(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.ListViewUpdatedEventArgs) Handles lViewModifierCours.ItemUpdated
+        mViewCours.ActiveViewIndex = 0
+        lViewCours.DataBind()
+    End Sub
+
 End Class
