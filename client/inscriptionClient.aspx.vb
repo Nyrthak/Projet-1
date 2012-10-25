5acf419d00cfbed45c3e8bb216faee10f317c0d6
Lawrence Dubé <law_dube8@hotmail.com>
2012-10-25 10:46:36 -0400

    Ajout inscription.vb

--- client/inscriptionClient.aspx.vb
+++ client/inscriptionClient.aspx.vb
@@ -5,7 +5,7 @@ Partial Class client_inscriptionClient
     Private Shared lecontext As ModelContainer = Nothing
 
     Protected Sub dsContextCreating(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.EntityDataSourceContextCreatingEventArgs) _
-    Handles entityDataSourcePaiement.ContextCreating
+    Handles entiDataSourceMembre.ContextCreating
 
         'RÉCUPÈRE LE CONTEXTE DE FACON À N'EN AVOIR QU'UN
         If Not lecontext Is Nothing Then
@@ -15,7 +15,7 @@ Partial Class client_inscriptionClient
 
 
     Protected Sub dsContextDisposing(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.EntityDataSourceContextDisposingEventArgs) _
-    Handles entityDataSourcePaiement.ContextDisposing
+    Handles entiDataSourceMembre.ContextDisposing
         e.Cancel = True
     End Sub
 
@@ -23,30 +23,11 @@ Partial Class client_inscriptionClient
         lecontext = New ModelContainer()
     End Sub
 
+    Protected Sub Page_PreInit(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.PreInit
+        verificationTypeUser(1)
+    End Sub
+
     Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
         lecontext = Nothing
-    End Sub
-
-    Protected Sub entityDataSourcePaiement_DataBinding(ByVal sender As Object, ByVal e As System.EventArgs) Handles entityDataSourcePaiement.DataBinding
-        
-    End Sub
-
-    'Protected Sub entityDataSourcePaiement_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles entityDataSourcePaiement.Load
-    '    Dim noCompte As Integer = Session("noCompte")
-    '    Dim membreList = (From monMembre In lecontext.MembreSet Where monMembre.Compte.noCompte = noCompte)
-    '    Dim membreListString As String = ""
-    '    Dim listMembre As New Parameter
-    '    Dim noMembre As Integer
-    '    For Each membre In membreList
-    '        membreListString += (membre.noMembre & " ")
-    '    Next
-    '    listMembre.DefaultValue = membreListString
-    '    entityDataSourcePaiement.WhereParameters.Add(listMembre)
-
-    '    entityDataSourcePaiement.Where() = "It.membre.noMembre Like membreListString"
-    'End Sub
-
-    Protected Sub repeater_ItemDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.RepeaterItemEventArgs) Handles repeater.ItemDataBound
-        e.Item.Visible = True
     End Sub
 End Class
